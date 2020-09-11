c61f2ca609ae0164627456220f23e72d3be6e2dd
Eugene Molotov <em92@users.noreply.github.com>
2020-09-11 11:02:55 +0500

    Revert "Use os.PathLike in StaticFiles for directory (#1007)"
    This reverts commit 93878323e57e0bab92b4622849c67f5a7c96b24e.

--- starlette/responses.py
+++ starlette/responses.py
@@ -3,10 +3,9 @@ import http.cookies
 import json
 import os
 import stat
-import sys
 import typing
 from email.utils import formatdate
-from mimetypes import guess_type as mimetypes_guess_type
+from mimetypes import guess_type
 from urllib.parse import quote, quote_plus
 
 from async_generator import isasyncgen
@@ -30,15 +29,6 @@ try:
     import ujson
 except ImportError:  # pragma: nocover
     ujson = None  # type: ignore
-
-
-# Compatibility wrapper for `mimetypes.guess_type` to support `os.PathLike` on <py3.8
-def guess_type(
-    url: typing.Union[str, "os.PathLike[str]"], strict: bool = True
-) -> typing.Tuple[typing.Optional[str], typing.Optional[str]]:
-    if sys.version_info < (3, 8):  # pragma: no cover
-        url = os.fspath(url)
-    return mimetypes_guess_type(url, strict)
 
 
 class Response:
@@ -250,7 +240,7 @@ class FileResponse(Response):
 
     def __init__(
         self,
-        path: typing.Union[str, "os.PathLike[str]"],
+        path: str,
         status_code: int = 200,
         headers: dict = None,
         media_type: str = None,
